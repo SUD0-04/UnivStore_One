d80e3fe715ac91ed8812388d0920a3ca37397fb7
Arnav <umi0236@icloud.com>
2023-09-01 23:23:57 +0900

    Initial Commit

---
+++ UnivStore_One/UnivStore_OneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnivStore_OneApp.swift
+//  UnivStore_One
+//
+//  Created by Kngmin Kang on 2023/09/01.
+//
+
+import SwiftUI
+
+@main
+struct UnivStore_OneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
